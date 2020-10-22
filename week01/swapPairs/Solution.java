866a67a3a1506bf136d68a0b5369144325465c96
RowinaDu <1103353781@qq.com>
2020-10-22 09:54:12 +0800

    [24]两两交换链表中的节点

---
+++ week01/swapPairs/Solution.java
@@ -0,0 +1,69 @@
+package week01.swapPairs;
+//给定一个链表，两两交换其中相邻的节点，并返回交换后的链表。
+//
+// 你不能只是单纯的改变节点内部的值，而是需要实际的进行节点交换。
+//
+//
+//
+// 示例 1：
+//
+//
+//输入：head = [1,2,3,4]
+//输出：[2,1,4,3]
+//
+//
+// 示例 2：
+//
+//
+//输入：head = []
+//输出：[]
+//
+//
+// 示例 3：
+//
+//
+//输入：head = [1]
+//输出：[1]
+//
+//
+//
+//
+// 提示：
+//
+//
+// 链表中节点的数目在范围 [0, 100] 内
+// 0 <= Node.val <= 100
+//
+// Related Topics 链表
+// 👍 720 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+        ListNode dummyHead = new ListNode();
+        dummyHead.next = head;
+        ListNode temp = dummyHead;
+        while (temp.next != null && temp.next.next != null) {
+            ListNode node1 = temp.next;
+            ListNode node2 = temp.next.next;
+            temp.next = node2;
+            node1.next = node2.next;
+            node2.next = node1;
+            temp = node1;
+        }
+        return dummyHead.next;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
