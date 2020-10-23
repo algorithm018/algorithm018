2d80c759f9dcdadcb2cafa1bf5dc3d4dc3a39891
RowinaDu <1103353781@qq.com>
2020-10-23 10:05:25 +0800

    [21]合并两个有序链表

--- week01/mergeTwoLists/Solution.java
+++ week01/mergeTwoLists/Solution.java
@@ -41,5 +41,17 @@ class Solution {
         pre.next = l1 != null ? l2 : l1;
         return dummyNode.next;
     }
+
+    public ListNode mergeTwoLists1(ListNode l1, ListNode l2) {
+        if (l1 == null) return l2;
+        if (l2 == null) return l1;
+        if (l1.val < l2.val) {
+            l1.next = mergeTwoLists(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = mergeTwoLists(l1, l2.next);
+            return l2;
+        }
+    }
 }
 //leetcode submit region end(Prohibit modification and deletion)
