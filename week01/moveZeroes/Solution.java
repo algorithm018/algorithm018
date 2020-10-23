2721cbb8c6a56aa830cbc6cba98c8f5e0469b225
RowinaDu <1103353781@qq.com>
2020-10-23 11:22:31 +0800

    283 零移动

--- week01/moveZeroes/Solution.java
+++ week01/moveZeroes/Solution.java
@@ -28,5 +28,32 @@ class Solution {
             }
         }
     }
+
+    public void moveZeroes1(int[] nums) {
+        //双指针
+        //第一遍将非0元素归位
+        int j=0;
+        for (int i=0;i<nums.length;i++){
+            if(nums[i]!=0){
+                nums[j++] = nums[i];
+            }
+        }
+        //第二遍将末尾置为0
+        for (int i=j;i<nums.length;i++){
+            nums[i] = 0;
+        }
+    }
+
+    public void moveZeroes2(int[] nums) {
+        int j = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                int t = nums[i];
+                nums[i] = nums[j];
+                nums[j++] = t;
+            }
+        }
+    }
+
 }
 //leetcode submit region end(Prohibit modification and deletion)
