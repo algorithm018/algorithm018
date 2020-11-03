9956aed16f26ec70b29712f16845037162211ffb
RowinaDu <1103353781@qq.com>
2020-11-03 10:44:20 +0800

    [236]二叉树的最近公共祖先

---
+++ week03/lowestCommonAncestor/Solution.java
@@ -0,0 +1,102 @@
+package week03.lowestCommonAncestor;
+
+//给定一个二叉树, 找到该树中两个指定节点的最近公共祖先。
+//
+// 百度百科中最近公共祖先的定义为：“对于有根树 T 的两个结点 p、q，最近公共祖先表示为一个结点 x，满足 x 是 p、q 的祖先且 x 的深度尽可能大（
+//一个节点也可以是它自己的祖先）。”
+//
+// 例如，给定如下二叉树: root = [3,5,1,6,2,0,8,null,null,7,4]
+//
+//
+//
+//
+//
+// 示例 1:
+//
+// 输入: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 1
+//输出: 3
+//解释: 节点 5 和节点 1 的最近公共祖先是节点 3。
+//
+//
+// 示例 2:
+//
+// 输入: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 4
+//输出: 5
+//解释: 节点 5 和节点 4 的最近公共祖先是节点 5。因为根据定义最近公共祖先节点可以为节点本身。
+//
+//
+//
+//
+// 说明:
+//
+//
+// 所有节点的值都是唯一的。
+// p、q 为不同节点且均存在于给定的二叉树中。
+//
+// Related Topics 树
+// 👍 812 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+
+    Map<Integer, TreeNode> parent = new HashMap();
+    Set<Integer> visited = new HashSet<>();
+
+
+    public void dfs(TreeNode root) {
+        if (root.left != null) {
+            parent.put(root.left.val, root);
+            dfs(root.left);
+        }
+        if (root.right != null) {
+            parent.put(root.right.val, root);
+            dfs(root.right);
+        }
+    }
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        dfs(root);
+        while (p != null) {
+            visited.add(p.val);
+            p = parent.get(p.val);
+        }
+        while (q != null) {
+            if (visited.contains(q.val)) {
+                return q;
+            }
+            visited.add(q.val);
+            q = parent.get(q.val);
+        }
+        return null;
+    }
+
+
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
