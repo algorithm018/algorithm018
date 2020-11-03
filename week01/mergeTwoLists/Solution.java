0ef9e803fb8b7aead3565c5d2d5ab60aa64a9743
RowinaDu <1103353781@qq.com>
2020-11-03 10:45:43 +0800

    ~

--- week01/mergeTwoLists/Solution.java
+++ week01/mergeTwoLists/Solution.java
@@ -54,4 +54,21 @@ class Solution {
         }
     }
 }
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
 //leetcode submit region end(Prohibit modification and deletion)
