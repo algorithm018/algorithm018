9079461c03657ae73e16afb510c37321512c0835
RowinaDu <1103353781@qq.com>
2020-11-04 16:05:13 +0800

    [剑指 Offer 06]从尾到头打印链表

---
+++ week03/reversePrint/Solution.java
@@ -0,0 +1,59 @@
+package week03.reversePrint;
+
+//输入一个链表的头节点，从尾到头反过来返回每个节点的值（用数组返回）。
+//
+//
+//
+// 示例 1：
+//
+// 输入：head = [1,3,2]
+//输出：[2,3,1]
+//
+//
+//
+// 限制：
+//
+// 0 <= 链表长度 <= 10000
+// Related Topics 链表
+// 👍 69 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+import java.util.Stack;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public int[] reversePrint(ListNode head) {
+        Stack<Integer> stack = new Stack<>();
+        ListNode temp = head;
+        while (temp != null) {
+            stack.push(temp.val);
+            temp = temp.next;
+        }
+        int size = stack.size();
+        int[] res = new int[size];
+        for (int i = 0; i < size; i++) {
+            res[i] = stack.pop();
+        }
+        return res;
+    }
+}
+
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
